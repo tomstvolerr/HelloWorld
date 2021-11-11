fc820046a2b25cfa1843511840f62dc8a4294a19
tomstvolerr <90185802+tomstvolerr@users.noreply.github.com>
2021-11-12 00:22:05 +0300

    Добавлено 4 задание

--- src/com/ildar/idea/Zadanie3.java
+++ src/com/ildar/idea/Zadanie3.java
@@ -2,12 +2,12 @@ package com.ildar.idea;
 
 public class Zadanie3 {
     public static void main(String[] args) {
-        int[] myArray = new int[5];
-        myArray[0] = 1;
-        myArray[1] = 2;
-        myArray[2] = 3;
-        myArray[3] = 4;
-        myArray[4] = 5;
+        int[] myArray = {1, 2, 3, 4, 5};
+//        myArray[0] = 1;
+//        myArray[1] = 2;
+//        myArray[2] = 3;
+//        myArray[3] = 4;
+//        myArray[4] = 5;
         int length = myArray.length;
         swap(myArray,0,2);
         System.out.println(myArray[0] + myArray[2]);
